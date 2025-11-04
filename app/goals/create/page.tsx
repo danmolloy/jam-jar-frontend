f9feebc1c4a02506defe406f0393438f05b5aa47
Daniel Molloy <danielmolloy_6@icloud.com>
2025-11-04 11:44:32 +0000

    removed achievements and goals

--- app/goals/create/page.tsx
+++
@@ -1,7 +0,0 @@
-import { auth } from '@/auth';
-import CreateGoal from '@/components/goals/create';
-
-export default async function CreateGoalPage() {
-  const session = await auth();
-  return <CreateGoal mode="create" session={session!} />;
-}
