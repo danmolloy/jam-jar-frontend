1d313e32975cdf75e440a6c4ae70088bf8b92846
Daniel Molloy <danmolloy91@gmail.com>
2026-07-30 17:09:04 +1000

    updated operator

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
         </div>
       </div>
       <div className="flex flex-col md:flex-row justify-between w-full text-xs p-1 mt-4 min-h-[20px]">
-        <p>Operated by an Australian sole trader.</p>
+        <p>Jam Jar is operated by Team Mango Pty Ltd, Australia.</p>
         <p>© 2026 Jam Jar. All rights reserved.</p>
       </div>
     </footer>
